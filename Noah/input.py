a6f9b50e8f1a2caa226268c7eb35989fdca1d629
enejivk <enejivic@gmail.com>
2026-05-25 11:50:25 +0100

    input

---
+++ Noah/input.py
@@ -0,0 +1,26 @@
+# 4. Input and Output
+# I need your input means I need data from you
+# to get an input from a user in python we use the input() function
+
+# HOW TO USE THE INPUT FUNCTION
+# name = input("Enter you name ")
+
+# print(name)
+# data_type = type(name)
+# print(data_type)
+
+# all values that are return by input() function is alway a string
+
+
+# age = input("Enter your age ")
+# print(age)
+# data_type = type(age)
+# print(data_type)
+
+# write a code that get two numbers and add them together
+num1 = int(input("Enter num1 "))
+num2 = int(input("Enter num2 "))
+result = num1 + num2
+print(result)
+
+
